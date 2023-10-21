8cfc23f85e28609013621158a8c6ded69caa4c1b
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-21 20:24:37 +0530

    added user modal

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const http = require("http");
+const app = require("./app/app");
 
 //create server
 const PORT = process.env.PORT || 8080;
-const server = http.createServer();
+const server = http.createServer(app);
 server.listen(PORT, () => {
   console.log(`Server is running and  listening on ${PORT}`);
 });
