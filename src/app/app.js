0c4d29af19674914d7ed65f72b75e18014aa0c08
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-21 20:30:56 +0530

    connect  db

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const dbConnect = require("../config/dbConnect");
 require("dotenv").config();
 
 const app = express();
+//db connection
+dbConnect();
 
 module.exports = app;
