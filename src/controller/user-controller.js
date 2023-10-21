796b026370055822f2cb3451b0635e2aa2551ba5
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-21 21:04:32 +0530

    completed register api

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -1,4 +1,5 @@
 const User = require("../model/User");
+const bcrypt = require("bcryptjs");
 
 // @desc    Register user
 // @route   POST /api/v1/users/register
@@ -7,15 +8,27 @@ const User = require("../model/User");
 const register = async (req, res) => {
   const { fullname, email, password } = req.body;
 
+  //check if user is already registered
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    throw new Error("User already registered");
+  }
+
+  //hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
   //create the user
   const user = await User.create({
     fullname,
     email,
-    password,
+    password: hashedPassword,
   });
 
-  res.json({
-    msg: user,
+  res.status(201).json({
+    status: "success",
+    message: "User registerd successfully",
+    data: user,
   });
 };
 
