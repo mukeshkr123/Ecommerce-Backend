1b670976ed13e871eef34ead8d3dfab4038c1fe3
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-24 11:51:39 +0530

    added dummy getallproducts and getproudcts by id api

--- src/controller/product-controller.js
+++ src/controller/product-controller.js
@@ -38,4 +38,40 @@ const createProduct = asyncHandler(async (req, res) => {
   });
 });
 
-module.exports = createProduct;
+// @desc    Get all products
+// @route   GET /api/v1/products
+// @access  Public
+
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    status: "Success",
+    message: "Products fetched successfully",
+    products,
+  });
+});
+
+// @desc    Get single product
+// @route   GET /api/products/:id
+// @access  Public
+
+const getProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    throw new Error("Product not found");
+  }
+
+  res.status(200).json({
+    status: "success",
+    message: "Product fetched successfully",
+    product,
+  });
+});
+
+module.exports = {
+  createProduct,
+  getProducts,
+  getProduct,
+};
--- src/routes/v1/product-routes.js
+++ src/routes/v1/product-routes.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const createProduct = require("../../controller/product-controller");
+const {
+  createProduct,
+  getProducts,
+  getProduct,
+} = require("../../controller/product-controller");
 const isLoggedIn = require("../../middlewares/isLoggedIn");
 const router = express.Router();
 
 router.post("/", isLoggedIn, createProduct);
+router.get("/", getProducts);
+router.get("/:id", getProduct);
 
 module.exports = router;
