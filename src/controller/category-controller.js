8dbd956ed2aab68a6bb74671e9b32b6c7eb7b854
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-26 18:51:29 +0530

    Add get all , get single , update and delete category api

--- src/controller/category-controller.js
+++ src/controller/category-controller.js
@@ -7,25 +7,119 @@ const Category = require("../model/Category");
 const createCategory = asyncHandler(async (req, res) => {
   const { name } = req.body;
 
-  //check if category exists
-  const categoryExists = await Category.findOne({ name });
-  if (categoryExists) {
-    throw new Error(`Category ${name} already exists`);
+  try {
+    // Check if the category already exists
+    const categoryExists = await Category.findOne({ name });
+    if (categoryExists) {
+      throw new Error(`Category "${name}" already exists`);
+    }
+
+    // Create a new category
+    const category = await Category.create({
+      name,
+      user: req.userAuthId,
+    });
+
+    res.status(201).json({
+      status: "success",
+      message: "Category created successfully",
+      category,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message: error.message,
+    });
   }
+});
 
-  //create a new category
-  const category = await Category.create({
-    name,
-    user: req.userAuthId,
-  });
-
-  res.status(201).json({
+// @desc    Get All Categories
+// @route   GET /api/v1/categories
+// @access  Private/Admin
+const getAllCategories = asyncHandler(async (req, res) => {
+  const categories = await Category.find();
+  res.status(200).json({
     status: "success",
-    message: "Category created successfully",
-    category,
+    message: "Categories Fetched successfully",
+    categories,
   });
+});
+
+// @desc    Get single Category
+// @route   GET /api/v1/categories/:id
+// @access  Private/Admin
+const getSingleCategory = asyncHandler(async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.id);
+    if (!category) {
+      throw new Error("Category not found");
+    }
+    res.status(200).json({
+      status: "success",
+      message: "Category fetched successfully",
+      category,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+});
+
+// @desc    Update a Category
+// @route   PATCH /api/v1/categories/:id
+// @access  Private/Admin
+const updateCategory = asyncHandler(async (req, res) => {
+  try {
+    const category = await Category.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: req.body.name,
+      },
+      { new: true }
+    );
+    if (!category) {
+      throw new Error("Category not found");
+    }
+    res.status(200).json({
+      status: "success",
+      message: "Category updated successfully",
+      category,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+});
+
+// @desc    Delete a Category
+// @route   DELETE /api/v1/categories/:id
+// @access  Private/Admin
+const deleteCategory = asyncHandler(async (req, res) => {
+  try {
+    const category = await Category.findByIdAndDelete(req.params.id);
+    if (!category) {
+      throw new Error("Category not found");
+    }
+    res.status(200).json({
+      status: "success",
+      message: "Category deleted successfully",
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "error",
+      message: error.message,
+    });
+  }
 });
 
 module.exports = {
   createCategory,
+  getAllCategories,
+  getSingleCategory,
+  updateCategory,
+  deleteCategory,
 };
--- src/routes/v1/category-routes.js
+++ src/routes/v1/category-routes.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const { createCategory } = require("../../controller/category-controller");
+const {
+  createCategory,
+  getAllCategories,
+  getSingleCategory,
+  updateCategory,
+  deleteCategory,
+} = require("../../controller/category-controller");
 const isLoggedIn = require("../../middlewares/isLoggedIn");
 const router = express.Router();
 
 router.post("/", isLoggedIn, createCategory);
+router.get("/", isLoggedIn, getAllCategories);
+router.get("/:id", isLoggedIn, getSingleCategory),
+  router.delete("/:id", isLoggedIn, deleteCategory);
+router.put("/:id", isLoggedIn, updateCategory);
 
 module.exports = router;
